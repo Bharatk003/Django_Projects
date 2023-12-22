d5ad34e9a05602e51ddc59e6b7526335ddb8e912
Bharatk003 <bharatkshirsagar003@gmail.com>
2023-12-23 00:35:11 +0530

    22/12/2023 todays build

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello Bharat, You are at the poll index: ")
 
+def detail(request, question_id):
+    return HttpResponse("You'are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You'are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you'are voting on question %s." % question_id)
