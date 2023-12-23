ff7ad95cd2c1068339812eba60b4ae1739d5f8ec
Bharatk003 <bharatkshirsagar003@gmail.com>
2023-12-23 13:30:43 +0530

    adding templete in project

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Bharat, You are at the poll index: ")
+    # Query the Question model to get the latest five questions ordered by pub_date in descending order
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    
+    # Create a string by joining the question texts with commas
+    output = ",".join([q.question_text for q in latest_question_list])
+    
+    # Return an HTTP response with the comma-separated list of question texts
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You'are looking at question %s." % question_id)
